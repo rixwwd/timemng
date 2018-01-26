3ca7c93cff54b646f085018c35b62a2af099e543
rixwwd <rixwwd@users.noreply.github.com>
2018-01-26 23:32:52 +0900

    機能切り替えのタブを追加。

--- app/controllers/current_activity_controller.rb
+++ app/controllers/current_activity_controller.rb
@@ -10,8 +10,16 @@ class CurrentActivityController < ApplicationController
 
   # POST /current_activities/start
   def start
-    activity = Activity.find(params[:activity_id])
-    CurrentActivity.start(activity&.id)
+    begin
+      activity = Activity.find(params[:activity_id])  
+    rescue ActiveRecord::RecordNotFound
+      flush.now[:warn] = "Activity Not Selected."
+      respond_to do |format|
+        format.html { render :index }
+      end
+      return
+    end
+    CurrentActivity.start(activity.id)
     respond_to do |format|
       format.html { redirect_to current_activity_url }
     end
